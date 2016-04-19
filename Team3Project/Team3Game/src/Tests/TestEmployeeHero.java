9735278077f2c013f6a9133264aa4ece973ce780
ryanylander <nylander@udel.edu>
2016-04-19 13:09:05 -0400

    tests

--- Team3Project/Team3Game/src/Tests/TestEmployeeHero.java
+++ Team3Project/Team3Game/src/Tests/TestEmployeeHero.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TestEmployeeHero {
+import Model.EmployeeHero;
 
+public class TestEmployeeHero {
+	EmployeeHero hero = new EmployeeHero(0, 0);
 	@Test
 	public void test() {
 		fail("Not yet implemented");
