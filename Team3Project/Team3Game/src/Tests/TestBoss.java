053a257de4867c7b22da46d0064e59d1a053f505
ryanylander <nylander@udel.edu>
2016-04-19 13:31:45 -0400

    starting tests

--- Team3Project/Team3Game/src/Tests/TestBoss.java
+++ Team3Project/Team3Game/src/Tests/TestBoss.java
@@ -4,10 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Model.Boss;
+
 public class TestBoss {
 
 	@Test
 	public void test() {
+		Boss b = new Boss(0,0);
 		fail("Not yet implemented");
 	}
 
--- Team3Project/Team3Game/src/Tests/TestEmployeeHero.java
+++ Team3Project/Team3Game/src/Tests/TestEmployeeHero.java
@@ -4,10 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Model.EmployeeHero;
+
 public class TestEmployeeHero {
 
 	@Test
 	public void test() {
+		EmployeeHero hero = new EmployeeHero(0,0);
 		fail("Not yet implemented");
 	}
 
