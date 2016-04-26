6623be05e7cb91e1d2129245d29a4beaf21a92d2
John Howe <jhowe@udel.edu>
2016-04-26 01:29:36 -0400

    forgot some changes

--- Team3Project/Team3Game/src/Model/GamePlant.java
+++ Team3Project/Team3Game/src/Model/GamePlant.java
@@ -2,15 +2,30 @@ package Model;
 
 public class GamePlant {
 	boolean good;
+	int x;
+	int y;
+	String name;
 	
-	public GamePlant(boolean g)
+	
+	public GamePlant(String n, int x, int y, boolean g)
 	{
 		good = g;
+		this.x = x;
+		this.y = y;
+		this.name = n;
 	}
-	
+	public String getName(){return name;}
 	public boolean getGood()
 	{
 		return good;
+	}
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
 	}
 
 }
